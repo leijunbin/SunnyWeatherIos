b3527611afde07552e247b487d1d990b31f28ab6
Junbin Lei <1286561513@qq.com>
2022-01-17 17:44:15 +0800

    add .gitignore

--- Pods/Target Support Files/Pods-SunnyWeatherIos-SunnyWeatherIosUITests/Pods-SunnyWeatherIos-SunnyWeatherIosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SunnyWeatherIos_SunnyWeatherIosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SunnyWeatherIos_SunnyWeatherIosUITests
-@end
